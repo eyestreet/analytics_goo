340988ee8d1d12416de7d683d9949b007d4b7f0c
Rob Christie <robchristie@gmail.com>
2010-03-19 16:13:49 -0400

    Updated to shoulda instead of thoughtbot shoulda

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,6 +2,6 @@ require 'rubygems'
 require 'test/unit'
 require 'active_support'
 require 'active_support/test_case'
-gem 'thoughtbot-shoulda', ">= 2.10.1"
+gem 'shoulda', ">= 2.10.1"
 require 'shoulda'
 require 'mocha'
